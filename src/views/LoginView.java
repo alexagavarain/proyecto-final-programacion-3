01d3ccfaeb851e798724e2b2886d886626a30387
Alexa Gavarain <agavarain_24@alu.uabcs.mx>
2026-02-11 09:58:48 -0700

    Mensaje credenciales incorrectas y metodo para añadir espacios

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -1,12 +1,18 @@
 package views;
 
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JPasswordField;
@@ -17,69 +23,95 @@ public class LoginView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField emailTextField;
 	private JPasswordField passwordField;
+	boolean error;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginView frame = new LoginView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public LoginView() {
+		error = true;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 361, 430);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		createSpace(100, contentPane);
 		
-		textField = new JTextField();
-		textField.setBounds(143, 165, 137, 18);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		JLabel appName = new JLabel("Movies");
+		appName.setToolTipText("");
+		appName.setFont(new Font("Arial", Font.BOLD, 30));
+		appName.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(appName);
+		createSpace(60, contentPane);
 		
-		JButton btnNewButton = new JButton("Iniciar sesión");
-		btnNewButton.addActionListener(new ActionListener() {
+		JLabel loginText = new JLabel("Inicia sesión para acceder a nuestro catálago");
+		loginText.setToolTipText("");
+		loginText.setFont(new Font("Arial", Font.PLAIN, 16));
+		loginText.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(loginText);
+		createSpace(40, contentPane);
+		
+		JLabel emailLabel = new JLabel("Correo electrónico");
+		emailLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		emailLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 75));
+		emailLabel.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(emailLabel);
+		
+		emailTextField = new JTextField();
+		emailTextField.setMaximumSize(new Dimension(250, 25));
+		emailTextField.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(emailTextField);
+		emailTextField.setColumns(1);
+		emailTextField.setBorder(null);
+		createSpace(15, contentPane);
+
+		JLabel passwordLabel = new JLabel("Contraseña");
+		passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		passwordLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 117));
+		passwordLabel.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(passwordLabel);
+		
+		passwordField = new JPasswordField();
+		passwordField.setMaximumSize(new Dimension(250, 25));
+		passwordField.setAlignmentX(CENTER_ALIGNMENT);
+		passwordField.setBorder(null);
+		contentPane.add(passwordField);
+		
+		createSpace(3, contentPane);
+		
+		if (error) {
+			showError();
+		}
+		
+		createSpace(40, contentPane);
+		
+		
+		JButton loginButton = new JButton("Acceder");
+		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(105, 267, 130, 27);
-		contentPane.add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("Correo");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(67, 165, 47, 15);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblContrasea = new JLabel("Contraseña");
-		lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblContrasea.setBounds(55, 209, 78, 15);
-		contentPane.add(lblContrasea);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(143, 209, 137, 18);
-		contentPane.add(passwordField);
-		
-		JLabel lblNewLabel_1 = new JLabel(" Movies");
-		lblNewLabel_1.setToolTipText("");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 24));
-		lblNewLabel_1.setBounds(105, 53, 114, 49);
-		contentPane.add(lblNewLabel_1);
-
+		loginButton.setMaximumSize(new Dimension(250, 30));
+		loginButton.setBackground(Color.BLACK);
+		loginButton.setForeground(Color.WHITE);
+		loginButton.setFocusPainted(false);  
+		loginButton.setBorderPainted(false);
+		loginButton.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(loginButton);
 	}
+	
+	public void showError() {
+		JLabel errorLabel = new JLabel("*Credenciales incorrectas");
+		errorLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		errorLabel.setForeground(Color.RED);
+		errorLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 110));
+		errorLabel.setAlignmentX(CENTER_ALIGNMENT);
+		contentPane.add(errorLabel);
+	}
+	
+	public void createSpace(int pixels, JPanel container) {
+		container.add(Box.createVerticalStrut(pixels));
+	}
+	
 }
-
