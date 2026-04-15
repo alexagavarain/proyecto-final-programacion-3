4c3eb8919f901b4fca4bf6c2aed633c3d5c1c911
Nenii24 <erandenigomez12@gmail.com>
2026-04-15 00:31:01 -0700

    clase user

--- src/models/User.java
+++ src/models/User.java
@@ -1,38 +1,37 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class User {
 	
 	private String name;
 	private String email;
-	private String country;
-	private char gender;
-	private String description;
-	private List<String> languages;
+	private String career;
+	private char turno;
+	private char grupo;
 	private String password;
 	
-	public User() {
-		
-		languages = new ArrayList<>();
-	}
 	
 	public User(String email, String password) {
 		this.email = email;
 		this.password = password;
-		languages = new ArrayList<>();
+
 	}
-	
-	public User(String name, String email, String country, char gender, String description, List<String> languages) {
+	public User(String name, String email, String career, char turno, char grupo) {
+		this.email = email;
+		this.name = name;
+		this.career= career;
+		this.turno = turno;
+		this.grupo = grupo;
+
+	}
+	public User(String name, String email, String career, char turno, char grupo, String password) {
 		super();
 		this.name = name;
 		this.email = email;
-		this.country = country;
-		this.gender = gender;
-		this.description = description;
-		this.languages = languages;
+		this.career = career;
+		this.turno = turno;
+		this.grupo = grupo;
+		this.password = password;
 	}
 
 	public String getName() {
@@ -50,39 +49,27 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getCountry() {
-		return country;
+	public String getCareer() {
+		return career;
 	}
-
-	public void setCountry(String country) {
-		this.country = country;
+	public void setCareer(String career) {
+		this.career = career;
 	}
-
-	public char getGender() {
-		return gender;
+	public char getTurno() {
+		return turno;
 	}
-
-	public void setGender(char gender) {
-		this.gender = gender;
+	public void setTurno(char turno) {
+		this.turno = turno;
 	}
-
-	public String getDescription() {
-		return description;
+	public char getGrupo() {
+		return grupo;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	public void setGrupo(char grupo) {
+		this.grupo = grupo;
 	}
-
-	public List<String> getLanguages() {
-		return languages;
+	public void setPassword(String password) {
+		this.password = password;
 	}
-
-	public void setLanguages(List<String> languages) {
-		this.languages = languages;
-	}
-	
 	public String getPassword() {
 		return password;
 	}
@@ -90,20 +77,17 @@ public class User {
 	public String toString() {
 		return "Nombre: " + name +
 		           "\nEmail: " + email +
-		           "\nPaís: " + country +
-		           "\nGénero: " + gender +
-		           "\nDescripción: " + description +
-		           "\nLenguajes:\n" +
-		           String.join("\n", languages);
+		           "\nCareer: " +  career +
+		           "\nTurno: " + turno +
+		           "\nGrupo: " + grupo;
 	}
 	
 	public String toCsv() {
 		return name + "," +
 		           email + "," +
-		           country + "," +
-		           gender + "," +
-		           description + "," +
-		           String.join("|", languages);
+		           career + "," +
+		           turno + "," +
+		           grupo;
 	}
 	
 	public static User fromCsv(String userData) {
@@ -111,17 +95,12 @@ public class User {
 		
 		String name = data[0];
 		String email = data[1];
-	    String country = data[2];
-	    char gender = data[3].charAt(0);
-	    String description = data[4];
+	    String career = data[2];
+	    char turno = data[3].charAt(0);
+	    char grupo = data[4].charAt(0);
 	    
-	    List<String> languages = new ArrayList<String>();
-	    
-	    if(data.length > 5) {
-	    	languages = Arrays.asList(data[5].split("\\|"));
-	    }
-	    
-	    return new User(name, email, country, gender, description, languages);
+
+	    return new User(name, email, career, turno, grupo);
 		
 	}
 
