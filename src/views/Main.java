ade339efd5e079fa04fdcf9dda911950d791f3ee
Alexa Gavarain <agavarain_24@alu.uabcs.mx>
2026-05-05 20:17:05 -0700

    Librerias importadas de lib y clase Config

--- src/views/Main.java
+++ src/views/Main.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 
 import javax.swing.UIManager;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import controllers.HomeController;
@@ -15,7 +16,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		FlatLightLaf.setup();
+		FlatDarkLaf.setup();
 		
 		CreateFont.loadFont();
 		
